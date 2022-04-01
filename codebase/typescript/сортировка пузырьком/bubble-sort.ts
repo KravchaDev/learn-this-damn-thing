ce09eeb4af2ac470de0db93aab6a1a9e7b3b7b17
Akavvi <gabdushev.i@yandex.ru>
2022-04-01 20:16:53 +0500

    (feat) add typescript folder and bubble sort in ts

---
+++ codebase/typescript/сортировка пузырьком/bubble-sort.ts
@@ -0,0 +1,23 @@
+const swapItems = (items: Array<number>, firstIndex: number, secondIndex: number): void => {
+    let item = items[firstIndex]
+    items[firstIndex] = items[secondIndex]
+    items[secondIndex] = item
+}
+
+const bubbleSort = (items: Array<number>): Array<number> => {
+    for (let i = 0; i < items.length; i++) {
+        for (let j = 0, stopSort = items.length - i - 1; j < stopSort; j++) {
+         if(items[j] > items[j+1]) {
+             swapItems(items, j, j+1)
+         }
+        }
+    }
+    return items
+}
+
+
+const items = [1,5,3,2,6,7,2,53,1035,2034,2077]
+
+const result = bubbleSort(items)
+
+console.log(result)
